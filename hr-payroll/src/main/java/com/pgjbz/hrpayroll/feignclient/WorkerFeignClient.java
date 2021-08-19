9be33944ad8c3c94130697354c85496caf8bf336
Paulo Gabriel <pgj1085@gmail.com>
2021-08-18 21:39:30 -0300

    Move worker references to remote config

--- hr-payroll/src/main/java/com/pgjbz/hrpayroll/feignclient/WorkerFeignClient.java
+++ hr-payroll/src/main/java/com/pgjbz/hrpayroll/feignclient/WorkerFeignClient.java
@@ -1,11 +1,13 @@
 package com.pgjbz.hrpayroll.feignclient;
 
 import com.pgjbz.hrpayroll.model.Worker;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+@RefreshScope
 @FeignClient(name = "${services.hr-worker.name}",  path = "${services.hr-worker.path}")
 public interface WorkerFeignClient {
 
